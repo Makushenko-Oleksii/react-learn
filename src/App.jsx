874a0a2745baf6c3412940a9e00225a710d0a7b6
Oleksii Makushenko <makushenkooleksii@gmail.com>
2025-03-19 20:53:33 +0200

    add CardButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import CardButton from "./components/CardButton/CardButton";
 import JournalItem from "./components/JournalItem/JournalItem";
 
 function App() {
+  const data = [
+    {
+      title: "Подготовка к обновлению курсов",
+      text: "Горные походы открывают удивительные природные ландшафты...",
+      date: new Date().toLocaleDateString("ru-RU"),
+    },
+    {
+      title: "Поход в годы",
+      text: "Думал, что очень много времени...",
+      date: new Date().toLocaleDateString("ru-RU"),
+    },
+    {
+      title: "Первая заметка",
+      text: "Создал первую заметку, чтобы ...",
+      date: new Date().toLocaleDateString("ru-RU"),
+    },
+  ];
+
   return (
     <>
       <h1>Journal</h1>
       <Button />
-      <JournalItem />
+      <CardButton>
+        <JournalItem
+          title={data[0].title}
+          text={data[0].text}
+          date={data[0].date}
+        />
+      </CardButton>
+      <CardButton>
+        <JournalItem
+          title={data[1].title}
+          text={data[1].text}
+          date={data[1].date}
+        />
+      </CardButton>
+      <CardButton>
+        <JournalItem
+          title={data[2].title}
+          text={data[2].text}
+          date={data[2].date}
+        />
+      </CardButton>
     </>
   );
 }
--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,19 +1,12 @@
 import "./JournalItem.css";
-function JournalItem() {
-  const title = "Подготовка к обновлению курсов";
-  const date = new Date();
-  const formattedDate = date.toLocaleDateString("ru-RU");
-  const text = "Горные походы открывают удивительные природные ландшафты";
-
+function JournalItem({ title, date, text }) {
   return (
     <>
-      <div className="journal-item">
-        <h2 className="journal-item__header">{title}</h2>
-        <h2 className="journal-item__body">
-          <div className="journal-item__date">{formattedDate}</div>
-          <div className="journal-item__text">{text}</div>
-        </h2>
-      </div>
+      <h2 className="journal-item__header">{title}</h2>
+      <h2 className="journal-item__body">
+        <div className="journal-item__date">{date}</div>
+        <div className="journal-item__text">{text}</div>
+      </h2>
     </>
   );
 }
