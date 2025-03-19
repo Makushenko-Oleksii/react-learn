15b8f3fbcd3292fb096a8661c2fe9c9a81baac58
Oleksii Makushenko <makushenkooleksii@gmail.com>
2025-03-19 18:29:33 +0200

    create app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Button from "./components/Button/Button";
+import JournalItem from "./components/JournalItem/JournalItem";
+
+function App() {
+  return (
+    <>
+      <h1>Journal</h1>
+      <Button />
+      <JournalItem />
+    </>
+  );
+}
+
+export default App;
